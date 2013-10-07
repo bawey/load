fcf7cd2eb5fbf5d6b148374c10227c5e4d4d055f
bawey <tomek.bawej@gmail.com>
2013-10-07 18:23:18 +0200

    momnday

--- src/test/java/ch/cern/cms/load/taps/flashlist/AdHocTapTest.java
+++ src/test/java/ch/cern/cms/load/taps/flashlist/AdHocTapTest.java
@@ -1,5 +1,7 @@
 package ch.cern.cms.load.taps.flashlist;
 
+import java.util.Map;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -20,7 +22,7 @@ public class AdHocTapTest extends SwingTest {
 	ExpertController ec;
 	EventProcessor ep;
 	EventsTap tap;
-	private double pace = 100;
+	private double pace = 7;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -74,26 +76,77 @@ public class AdHocTapTest extends SwingTest {
 					}
 				});
 
-		ep.createEPL("create window Backpressure.win:keepall() as (bpFraction double, slotNumber String, linkNumber String, timestamp String)");
+		ep.createEPL("create window BackpressureFilter.std:unique(kontext, slotNumber, linkNumber) as (bpFraction double, kontext String, slotNumber String, linkNumber String, timestamp String)");
 		ep.createEPL("on pattern[every a=frlcontrollerLink(fifoAlmostFullCnt>0)->b=frlcontrollerLink(fifoAlmostFullCnt>0,context=a.context,slotNumber=a.slotNumber,sessionid=a.sessionid,clockCount>a.clockCount)]"
-				+ " insert into Backpressure select (b.fifoAlmostFullCnt-a.fifoAlmostFullCnt)/(b.clockCount-a.clockCount) as bpFraction, b.slotNumber as slotNumber, b.linkNumber as linkNumber, b.timestamp as timestamp");
+				+ " insert into BackpressureFilter select (b.fifoAlmostFullCnt-a.fifoAlmostFullCnt)/(b.clockCount-a.clockCount) as bpFraction, b.slotNumber as slotNumber, b.linkNumber as linkNumber, b.timestamp as timestamp, b.context as kontext");
 		// 2 back-to-back events with fifoAlmostFullCnt > 0.
 
-		ep.createEPL("create window PositiveBackpressure.win:keepall() as select * from Backpressure");
-		ep.createEPL("insert into PositiveBackpressure select * from Backpressure where bpFraction>0");
+		ep.createEPL("create window Backpressure.std:unique(kontext, linkNumber, slotNumber) as select * from BackpressureFilter");
 
-		ep.registerStatement("select * from PositiveBackpressure", new UpdateListener() {
+		// on every positive BackpressureFilter.bpFraction only if (kontext,
+		// link, slot) not yet giving backpressure
+		
+		
+		ep.registerStatement("on pattern[every bf=BackpressureFilter] select count(*) as backpressures4triplet from Backpressure where kontext=bf.kontext and linkNumber=bf.linkNumber and slotNumber=bf.slotNumber", watchUpdater);
+		
+		ep.createEPL("on BackpressureFilter(bpFraction>0) as bf insert into Backpressure select bf.* where (select count(*) as cnt from Backpressure where kontext=bf.kontext and linkNumber=bf.linkNumber and slotNumber=bf.slotNumber)=0");
+				
+
+		
+		/** playground **/
+		
+//		ep.registerStatement(
+//				"on BackpressureFilter(bpFraction>0) as bf insert into Backpressure select bf.*",
+//				new UpdateListener() {
+//					@Override
+//					public void update(EventBean[] newEvents, EventBean[] oldEvents) {
+//						console("BP update", "updating Backpressure");
+//						//AdHocTapTest.this.raiseAlarm(backpressureDesc(newEvents[0].getUnderlying()));
+//					}
+//				});
+		
+		/** playground ed **/
+		
+		
+		
+		
+		
+		
+		
+		ep.registerStatement("select count(*) as rowsInBackpressure from Backpressure", watchUpdater);
+
+
+		ep.registerStatement("select b.* from pattern[every b=Backpressure]", new UpdateListener() {
 			@Override
 			public void update(EventBean[] newEvents, EventBean[] oldEvents) {
-				AdHocTapTest.this.console("backpressure", newEvents[0].getUnderlying().toString());
+				console("debug", "type: " + newEvents[0].getUnderlying().toString());
+				AdHocTapTest.this.watch(backpressureDesc(newEvents[0].getUnderlying()),
+						((Map<?, ?>) newEvents[0].getUnderlying()).get("bpFraction").toString());
 			}
 		});
 
+		// should fire only if deleting... and does
+		ep.registerStatement(
+				"on BackpressureFilter(bpFraction<0.0000000000000000000000000000000000000000000000000000000000000001) as bf delete from Backpressure b where b.kontext=bf.kontext and b.slotNumber=bf.slotNumber and b.linkNumber=bf.linkNumber",
+				new UpdateListener() {
+					@Override
+					public void update(EventBean[] newEvents, EventBean[] oldEvents) {
+						AdHocTapTest.this.console("backpressure", "removing backpressure");
+						AdHocTapTest.this.cancelAlarm(backpressureDesc(newEvents[0].getUnderlying()));
+					}
+				});
 	}
 
 	/**
 	 * sum the rate in EVM as deltaN/deltaT for given session id
 	 */
+
+	private String backpressureDesc(Object underlying) {
+		Map<?, ?> meb = (Map<?, ?>) underlying;
+		StringBuffer sb = new StringBuffer("BP! [ctx:").append(meb.get("kontext")).append("|s:").append(meb.get("slotNumber"))
+				.append("|l:").append(meb.get("linkNumber")).append("]");
+		return sb.toString();
+	}
 
 	public void task1(final EventProcessor ep) {
 		/**
@@ -111,20 +164,30 @@ public class AdHocTapTest extends SwingTest {
 				+ " msec) as (subrate double, url String, lid String)");
 		ep.createEPL("insert into subrates select deltaN/deltaT as subrate, context as url, lid from EVM where sessionid=sid and deltaT>0");
 
+		/**
+		 * keep the last relevant subrate timestamp in a variable TODO: might
+		 * arrive out-of-order!
+		 **/
+		ep.createEPL("create variable String lastEVMtimestamp = ''");
+		ep.registerStatement("on EVM(sessionid=sid) as evm set lastEVMtimestamp=evm.timestamp", watchUpdater);
+
 		/** create a window to contain observed rates **/
 		ep.createEPL("create window rates.win:time(" + 60000 / pace + " sec) as (rate double)");
 		/** and fill it periodically **/
 		ep.registerStatement("on pattern[every timer:interval(" + (1000 / pace)
 				+ " msec)] insert into rates select sum(subrate) as rate from subrates", watchUpdater);
-
+		ep.registerStatement("select prior(1,rate) as trgRate, lastEVMtimestamp as timestamp from rates", watchUpdater);
 		/**
 		 * create variable for rate reference level (avg rate observed over last
 		 * minute)
 		 **/
 		ep.createEPL("create variable double avgRate=0");
-		ep.registerStatement("on rates set avgRate=Math.round((select avg(rate) from rates))", watchUpdater);
+		ep.createEPL("on rates set avgRate=Math.round((select avg(rate) from rates))");
 
-		ep.registerStatement("select avgRate, a.rate from pattern[every a=rates(rate>(avgRate*1.3) or rate < (avgRate*0.7))]",
+		ep.registerStatement("select avgRate, lastEVMtimestamp as timestamp from rates", watchUpdater);
+
+		ep.registerStatement(
+				"select avgRate, a.rate, lastEVMtimestamp as timestamp from pattern[every a=rates(rate>(avgRate*1.3) or rate < (avgRate*0.7))]",
 				new UpdateListener() {
 					@Override
 					public void update(EventBean[] newEvents, EventBean[] oldEvents) {
@@ -132,6 +195,9 @@ public class AdHocTapTest extends SwingTest {
 						ep.getRuntime().executeQuery("delete from rates");
 					}
 				});
+
+		ep.registerStatement("select count(*) as totalEVM from EVM", watchUpdater);
+
 	}
 
 	@After
